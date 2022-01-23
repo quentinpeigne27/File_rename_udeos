d5dafe023a154e886eaa25635a9df9268ecd7319
quentinpeigne27 <quentinpeignepro@gmail.com>
2022-01-23 17:56:05 +0100

    Update commentary

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import os
 import shutil
 import argparse
 
-#fonction for list files and directories in path of your choice
+#function for list files and directories in path of your choice
 def list_dir(location) :
     directory = os.listdir(location)
     for file in directory :
         print(f'{file}')
 
-#fonction for rename files            
+#function for rename files            
 def rename_file(location, file, new_file) :
     os.rename(location + file, location + new_file)  
 
-#fonction for create list of file
+#function for create list of file
 def enumerate_files(location) :
     directory = os.listdir(location)
     os.chdir(location)
@@ -20,11 +20,11 @@ def enumerate_files(location) :
         if os.path.isfile(file) == True:
             os.rename(file, str(i) + "_" + file)
 
-#fonction for zip files
+#function for zip files
 def zip_files(location, destination) :
     shutil.make_archive(location, 'zip', destination)
 
-#fonction for sort files
+#function for sort files
 def sort_files(location) :
     directory = os.listdir(location)
     os.chdir(location)
@@ -42,7 +42,7 @@ def sort_files(location) :
             os.mkdir(location + "jpg")
             shutil.move(file, location + "jpg")
 
-#fonction for remove filenames spaces           
+#function for remove filenames spaces           
 def remove_filename_space(location) :
     directory = os.listdir(location)
     os.chdir(location)
@@ -65,26 +65,26 @@ parser.add_argument("-rs", "--remove_space", action='store_true', help="Remove f
 parser.add_argument("-z","--zip", action='store_true', help="Zip a directory")
 args = parser.parse_args()
 
-#Call the fonction for list dir with -ld argument
+#Call the function for list dir with -ld argument
 if args.location and args.list_directory :
     list_dir(args.location)
 
-#Call the fonction for rename file with -f and -nf arguments
+#Call the function for rename file with -f and -nf arguments
 elif args.location and args.file and args.new_file :    
     rename_file(args.location, args.file, args.new_file)
 
-#Call the fonction for add a number in front of the file name to create a list
+#Call the function for add a number in front of the file name to create a list
 elif args.location and args.enumerate_directory :
     enumerate_files(args.location)
     
-#Call the fonction for ZIP files
+#Call the function for ZIP files
 elif args.location and args.zip and args.destination :
     zip_files(args.location, args.destination) 
 
-#Call the fonction for sorts files
+#Call the function for sorts files
 elif args.location and args.sort_file :
     sort_files(args.location)
     
-#Call the fonction for remove filenames spaces
+#Call the function for remove filenames spaces
 elif args.location and args.remove_space :
     remove_filename_space(args.location)
